a6e628ad09ac2def1567db1cba7dac55b545c3a6
romanolivi <romano_livi@outlook.com>
2021-03-30 18:10:55 -0400

    commit tree 6fd7dfededcf6ecfb6e468f5766b6d7dca71d916

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  };
+
+  static titleize(string) {
+    let str = string.split(' ');
+    let result = [];
+    let noCap = ["a", "an", "but", "from", "and", "of", "for", "at", "by", "the"];
+    
+    for (let i = 0; i < str.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(str[i]))
+      } else {
+        if (noCap.includes(str[i])) {
+          result.push(str[i])
+        } else {
+          result.push(this.capitalize(str[i]))
+        }
+      }
+    }
+    return result.join(' ');
+  };
 }
